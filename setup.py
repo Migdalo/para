9d5fa84b9ff159e26362a719b0b6617b6331a480
Migdalo <Migdalo@users.noreply.github.com>
2016-12-13 02:16:03 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import unittest
+
+def para_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
+
+setup(name='para',
+      version='0.1.1',
+      author='Migdalo',
+      license='MIT',
+      packages=['para'],
+      test_suite='setup.para_test_suite',
+      entry_points={
+        'console_scripts': [
+            'para = para.para:process_arguments'
+        ]
+      },
+      zip_safe=True)
+
