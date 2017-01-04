5a9c84661af82763c72b534eefa247f12fe307e3
Migdalo <Migdalo@users.noreply.github.com>
2017-01-05 00:09:52 +0200

    Add support for piping

--- para/para.py
+++ para/para.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python
-
-from convert import *
+#!/usr/bin/env python
 import argparse
 import logging
+import sys
+import ast
+import inspect
+import convert
 import cipher
 
 logger = logging.getLogger(__name__)
@@ -10,7 +12,6 @@ printoffset = 19
 
 """ Calls a function received as a parameter and logs its result. """
 def log_event(my_function, convertable, printable_text):
-    global printoffset
     formatstring = '{0: <' + str(printoffset) +'}'
     result = my_function(convertable)
     
@@ -31,48 +32,58 @@ def log_event(my_function, convertable, printable_text):
 
 """ Calls log_event for each dunction in the convert module. """
 def print_results(convertable):
-    global printoffset
     formatstring = '{0: <' + str(printoffset) +'}'
     logger.warn(formatstring.format('Action')+ ' | ' + 'Result')
     logger.warn('-' * printoffset * 3)
-    logevent = formatstring.format('User input ') + ' | ' + convertable
+    logevent = formatstring.format('User input ') + ' | ' + str(convertable)
     logger.debug(logevent)
-    log_event(hex_to_ascii, (convertable), 'Hex to ascii')
-    log_event(ascii_to_hex, (convertable), 'Ascii to hex')
-    log_event(encode_base64, (convertable), 'Encode base64')
-    log_event(decode_base64, (convertable), 'Decode base64')
-    log_event(integer_to_hex, (convertable), 'Decimal to hex')
-    log_event(hex_to_integer, (convertable), 'Hex to decimal')
-    log_event(integer_to_ascii, (convertable), 'Decimal to ascii')
-    log_event(ascii_to_integer, (convertable), 'Ascii to decimal')
-    log_event(ascii_to_binary, (convertable), 'Ascii to binary')
-    log_event(integer_to_binary, (convertable), 'Decimal to binary')
-    log_event(binary_to_int, (convertable), 'Binary to decimal')
-    log_event(binary_to_ascii, (convertable), 'Binary to ascii')
+    
+    # Get list of functions in the convert module and call them
+    convert_functions = inspect.getmembers(convert, inspect.isfunction)
+    for f in convert_functions:
+        title = f[0].replace('_', ' ').capitalize()
+        log_event(f[1], (convertable), title)
     log_event(cipher.rotate, (convertable), 'ROT13')
-
+    
 """ Initiate logging """
 def set_logging(verbose, quiet):
-    ch = logging.StreamHandler()
-    logger.addHandler(ch)
+    ch = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(message)s'.strip())
+    ch.setFormatter(formatter)
+    
     if verbose:
         logger.setLevel(logging.DEBUG)
     elif quiet:
         logger.setLevel(logging.CRITICAL)
     else:
         logger.setLevel(logging.WARN)
+        
+    logger.addHandler(ch)
 
 """ Creates command-line interface and processes arguments. """
 def process_arguments():
-    parser = argparse.ArgumentParser(description='Converts strings and numbers to other types.',\
+    parser = argparse.ArgumentParser( \
+        description='Converts strings and numbers to other types.',\
         epilog='Author: Migdalo (https://github.com/Migdalo)')
-    parser.add_argument('convertable', help='String or number you want to convert.')
+    parser.add_argument('convertable', nargs='?', default='', \
+        help='String or number you want to convert.')
     parser.add_argument('-r', '--rot', action='store_true', \
         help='Print more rotation cipher results.')
     verbosity_group = parser.add_mutually_exclusive_group()
-    verbosity_group.add_argument('-v', '--verbose', action='store_true', help='Use verbose mode.')
-    verbosity_group.add_argument('-q', '--quiet', action='store_true', help='Use quiet mode.')
+    verbosity_group.add_argument('-v', '--verbose', action='store_true', \
+        help='Use verbose mode.')
+    verbosity_group.add_argument('-q', '--quiet', action='store_true', \
+        help='Use quiet mode.')
+        
     args = parser.parse_args()
+    
+    if not args.convertable:
+        if not sys.__stdin__.isatty():
+            line = sys.__stdin__.readline().strip()
+            args.convertable += str(ast.literal_eval('"' + line + '"'))
+            args.convertable.strip()
+        else:
+            raise parser.error("error: too few arguments")
     
     set_logging(args.verbose, args.quiet)
     if args.rot:
