7c9fa350365a30a7cc3f270fa324b82d1201f482
Migdalo <Migdalo@users.noreply.github.com>
2017-01-12 19:19:30 +0200

    Update para.py

--- para/para.py
+++ para/para.py
@@ -10,33 +10,31 @@ import cipher
 logger = logging.getLogger(__name__)
 printoffset = 19
 
-""" Calls a function received as a parameter and logs its result. """
-def log_event(my_function, convertable, printable_text):
-    formatstring = '{0: <' + str(printoffset) +'}'
-    result = my_function(convertable)
+""" Calls a function received as a parameter and logs the result. """
+def log_event(convertion_function, convertable, printable_text):
+    formatstring = '{0: <' + str(printoffset) + '}'
+    try:
+        result = convertion_function(convertable)
+    # Capture errors from functions which require wrong number of parameters
+    except TypeError: 
+        logger.debug('Failed to run function: ' + printable_text)
+        return
     
-    # Quiet mode: print only the result
-    if logger.getEffectiveLevel() is 50:
-        if result:
-            logger.critical(str(result))
-        else:
-            logger.debug(str(result))
-    
-    # Otherwise print the table
-    else:
+    # If not Quiet mode: print the table
+    if not logger.getEffectiveLevel() is 50:
         logevent = formatstring.format(printable_text) + ' | ' + str(result)
-        if result:
-            logger.critical(logevent)
-        else:
-            logger.debug(logevent)
-
+    if result:
+        logger.critical(logevent)
+    else:
+        logger.info(logevent)
+        
 """ Calls log_event for each function in the convert module. """
 def print_results(convertable):
-    formatstring = '{0: <' + str(printoffset) +'}'
-    logger.warn(formatstring.format('Action')+ ' | ' + 'Result')
+    formatstring = '{0: <' + str(printoffset) + '}'
+    logger.warn(formatstring.format('Action') + ' | ' + 'Result')
     logger.warn('-' * printoffset * 3)
     logevent = formatstring.format('User input ') + ' | ' + str(convertable)
-    logger.debug(logevent)
+    logger.info(logevent)
     
     # Get list of functions in the convert module and call them
     convert_functions = inspect.getmembers(convert, inspect.isfunction)
@@ -44,20 +42,20 @@ def print_results(convertable):
         title = f[0].replace('_', ' ').capitalize()
         log_event(f[1], (convertable), title)
     log_event(cipher.rotate, (convertable), 'ROT13')
-    
-""" Initiate logging """
+
+""" Initiates logging """
 def set_logging(verbose, quiet):
     ch = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter('%(message)s'.strip())
     ch.setFormatter(formatter)
     
     if verbose:
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
     elif quiet:
         logger.setLevel(logging.CRITICAL)
     else:
         logger.setLevel(logging.WARN)
-        
+    
     logger.addHandler(ch)
 
 """ Creates command-line interface and processes arguments. """
@@ -74,7 +72,6 @@ def process_arguments():
         help='Use verbose mode.')
     verbosity_group.add_argument('-q', '--quiet', action='store_true', \
         help='Use quiet mode.')
-        
     args = parser.parse_args()
     
     if not args.convertable:
