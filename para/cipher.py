ed18a4209a0e08c873ad5707d68031a911b58316
Migdalo <Migdalo@users.noreply.github.com>
2017-01-15 06:07:55 +0200

    Improve code quality

--- para/cipher.py
+++ para/cipher.py
@@ -1,27 +1,32 @@
+"""
+Cipher module encodes and decodes rotation cipher.
+"""
+from __future__ import print_function
 import string
 
-""" Get an alphabet range to use. """
 def discover_alphabet(convertable, nonumbers=False, addpunct=False):
+    """ Get an alphabet range to use. """
+    convertable = str(convertable)
     alphabet = []
     has_upper = False
     has_lower = False
     has_digits = False
     punct = []
-    for c in convertable:
-        if c not in alphabet:
-            if c in string.ascii_lowercase:
+    for char in convertable:
+        if char not in alphabet:
+            if char in string.ascii_lowercase:
                 has_lower = True
-            elif c in string.ascii_uppercase:
+            elif char in string.ascii_uppercase:
                 has_upper = True
-            elif not nonumbers and c in string.digits:
+            elif not nonumbers and char in string.digits:
                 has_digits = True
-            elif addpunct and c not in string.digits:
-                punct.append(c)
+            elif addpunct and char not in string.digits:
+                punct.append(char)
         if has_digits and has_lower and has_upper:
             break
         elif nonumbers and has_lower and has_upper:
             break
-            
+
     if has_lower:
         alphabet.append(string.ascii_lowercase)
     if has_upper:
@@ -32,40 +37,36 @@ def discover_alphabet(convertable, nonumbers=False, addpunct=False):
         alphabet.append(''.join(punct))
     return ''.join(alphabet)
 
-""" Caesar cipher """
 def rotate(convertable, alphabet='', rot=13):
+    """ Caesar cipher """
     plain = []
-    
+
     if not alphabet:
         alphabet = discover_alphabet(convertable)
-    
+
     if rot > len(alphabet):
         rot = rot - len(alphabet)
-    
-    try:
-        for c in convertable:
-            if c not in alphabet:
-                plain.append(c)
-            else:
-                tmp = alphabet.index(c) + rot
-                if tmp >= len(alphabet):
-                    tmp = tmp - len(alphabet)
-                plain.append(alphabet[tmp])
-        return ''.join(plain)
-    except:
-        return ''
 
-""" Print all possible rotations. """
+    for char in convertable:
+        if char not in alphabet:
+            plain.append(char)
+        else:
+            tmp = alphabet.index(char) + rot
+            if tmp >= len(alphabet):
+                tmp = tmp - len(alphabet)
+            plain.append(alphabet[tmp])
+    return ''.join(plain)
+
 def solve_rotation(convertable, keyword=''):
+    """ Print all possible rotations. """
     alphabet = discover_alphabet(convertable)
-    print '{0: <5}'.format('ROT'), convertable
-    print '-' * 20
+    print('{0: <5}'.format('ROT') + convertable)
+    print('-' * 20)
     for i in range(len(alphabet)):
         if not keyword:
-            print '{0: <5}'.format(str(i)), rotate(convertable, alphabet, i)
+            print('{0: <5}'.format(str(i)) + rotate(convertable, alphabet, i))
         else:
             solution = rotate(convertable, alphabet, i)
             if keyword in solution:
-                print '{0: <5}'.format(str(i)), solution
+                print('{0: <5}'.format(str(i)) + solution)
                 return
-
